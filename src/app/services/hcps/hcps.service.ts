5c2f111aa14dc0eda31faa14512dc345e53aef36
GeLoader <121944207+GeLoader@users.noreply.github.com>
2024-02-06 23:53:59 +0800

    a

--- src/app/services/hcps/hcps.service.ts
+++ src/app/services/hcps/hcps.service.ts
@@ -7,29 +7,9 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class HcpsService {
-  server = environment.server;
+  //server = environment.server;
   table = 'hcps';
   constructor(private http: HttpClient) { }
-
-  public post(data: any) {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        authorization: `Bearer `,
-      }),
-    };
-    return this.http
-      .post(`${this.server}${this.table}`, data)
-      .pipe(map((response) => response as any));
-  }
-
-  public get() {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        authorization: `Bearer `,
-      }),
-    };
-    return this.http
-      .get(`${this.server}${this.table}`)
-      .pipe(map((response) => response as any));
-  }
+ 
+   
 }

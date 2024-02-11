e965f6784b8cf7b250c1e1b9a84ebffec28aa31c
GeLoader <121944207+GeLoader@users.noreply.github.com>
2024-02-12 00:57:29 +0800

    test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   // apiBaseUrl: 'https://amselj.netlify.app/app.js',
     //apiBaseUrl: 'http://localhost:8080/api',
-    apiBaseUrl: 'https://a-med-nodejs.onrender.com/api',
+     apiBaseUrl: 'https://a-med-nodejs.onrender.com/api',
  // server: 'http://localhost:3005/',
 };
